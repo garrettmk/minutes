d43af62562c07fdd9d87af7e12b620f94e17b41c
Garrett Myrick <garrettmyrick@gmail.com>
2020-12-27 16:58:34 -0800

    Updated App component + tests

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,27 +1,89 @@
 import React from 'react';
-import Theme from '../Theme';
 import styled from 'styled-components/macro';
+import Button from '../components/Button';
+import CircularProgress from '../components/CircularProgress';
+import DurationInput from '../components/DurationInput';
+import Label from '../components/Label';
+import { useTimer } from '../hooks';
+import Theme from '../Theme';
 
-
-const Screen = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 100%;
-  height: 100%;
-  max-width: 350px;
-  max-height: 900px;
-  transform: translate(-50%, -50%);
-  border: 1px dotted white;
-`;
 
 
 function App() {
+  const timer = useTimer();
+  const completionRatio = timer.state === 'stopped' ? 1 : (timer.currentTick || 0) / timer.ticks;
+  const setDuration = (event: React.FormEvent<HTMLInputElement>) => {
+    const value = parseInt(event.currentTarget.value);
+    timer.setDuration(value);
+    timer.setTicks(value * 60);
+  };
+  
   return (
     <Theme>
-      <Screen>
-        Hey
-      </Screen>
+      <div css={`
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      `}>
+        <div css={`
+          position: relative;
+          width: 350px;
+          height: 350px;
+        `}>
+          <CircularProgress
+            width={350}
+            height={350}
+            ratio={completionRatio}
+          />
+          <div
+            css={`
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+            `}
+          >
+            <DurationInput
+              readOnly={timer.state !== 'stopped'}
+              value={timer.state === 'stopped' ? timer.duration : timer.remainingDuration}
+              onChange={setDuration}
+              css={`width: 180px;`}
+            />
+            <Label
+              variant='body'
+              color='secondary'
+              css={`
+                margin-top: 8px;
+                position: relative;
+                left: 8px;
+                letter-spacing: 16px;
+              `}
+            >
+              minutes
+            </Label>
+          </div>
+        </div>
+        <div css={`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 24px;
+        `}>
+          {timer.state === 'stopped' ? (
+            <Button onClick={timer.start}>
+              Start
+            </Button>
+          ) : (
+            <Button onClick={timer.reset}>
+              Reset
+            </Button>
+          )}
+        </div>
+      </div>
     </Theme>
   );
 }
