5ff91d5957228cb85cdefbe63d223c86734efded
Garrett Myrick <garrettmyrick@gmail.com>
2020-12-27 16:56:09 -0800

    Updated global typings

--- src/@types/react-robot/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node"/>
-
-declare module 'react-robot' {
-  import { Machine, MachineState, SendFunction, Service } from 'robot3';
-
-  export function useMachine(machine: Machine, initialContext: any) : [{ name: string, context: any }, SendFunction, Service];
-}
