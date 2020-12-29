4a558f14d140acefc8464b225e787ad110ea525d
Garrett Myrick <garrettmyrick@gmail.com>
2020-12-29 14:40:10 -0800

    Updated tsconfig for absolute paths

---
+++ src/test-utils/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { generateImage } from 'jsdom-screenshot';
+import Theme from '../Theme';
+
+type RenderParams = Parameters<typeof render>;
+
+const customRender = (ui: RenderParams[0], options?: RenderParams[1]) => render(ui, { wrapper: Theme, ...options })
+
+
+export * from '@testing-library/react';
+export { customRender as render }
+export { generateImage };
