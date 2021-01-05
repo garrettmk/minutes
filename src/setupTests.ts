3b0888837af1edd0c94fbb15a1e1c34a2cd71f67
Garrett Myrick <garrettmyrick@gmail.com>
2021-01-05 13:45:05 -0800

    Removed references to jest-image-snapshot

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,5 +4,3 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
-expect.extend({ toMatchImageSnapshot });
